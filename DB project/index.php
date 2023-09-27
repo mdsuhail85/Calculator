e61afb6a8dcbb4c5dc8c4aface7a33fd3571e871
mdsuhail85 <m.suhail8527@gmail.com>
2023-09-27 16:27:34 +0530

    marge all files

---
+++ DB project/index.php
@@ -0,0 +1,21 @@
+<?php 
+$server = "localhost";
+$username = "root";
+$password = "";
+
+$con = mysqli_connect($server, $username, $password);
+
+if(!$con){
+    die("connection to this database failed due to" .mysqli_connect_error());
+}
+// echo "Success connecting to the db";
+
+$name = $_POST['name'];
+$gender = $_POST['gender'];
+$age = $_POST['age'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$desc = $_POST['desc'];
+$sql = "INSERT INTO `trip` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());";
+
+?>
